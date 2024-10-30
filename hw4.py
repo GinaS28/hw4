3b36b6cf47208d5e57d68e779d9fd3b3f4c445c5
GinaS28 <reginasss2304@gmail.com>
2024-10-30 12:14:09 +0500

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,5 @@
+completed_homework_count = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = hours_spent/completed_homework_count
+print(course_name,', всего задач:',completed_homework_count,', затрачено часов: ',hours_spent,', среднее время выполнения ', time_per_task)
